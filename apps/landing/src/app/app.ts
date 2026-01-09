68024c9b3abadbb0eae26df629edc4162912a72b
yunakmykhailo <bogoon87@gmail.com>
2026-01-09 15:54:52 +0200

    update tailwind css

--- apps/landing/src/app/app.ts
+++ apps/landing/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiKit } from '@gmi-integrations/ui-kit';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, UiKit],
   selector: 'gmi-root',
   template: `
-    welcom
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <ui-kit />
     <router-outlet></router-outlet>
   `,
 })
