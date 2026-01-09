92df0553b63c46e0a9413e4eff0200e1623411be
yunakmykhailo <bogoon87@gmail.com>
2026-01-09 14:47:39 +0200

    init project

--- apps/landing/eslint.config.mjs
+++ apps/landing/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'gmi',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'gmi',
           style: 'kebab-case',
         },
       ],
